ed6da1c8434750db626df6d4a729e472ddf582cf
Alex Konichek <Konichekalex@gmail.com>
2020-06-15 10:10:26 +0300

    add validations

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import store from "./store";
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/database";
-
+import vuelidate from "vuelidate";
 import AppDate from "@/components/AppDate";
+
+Vue.use(vuelidate);
 Vue.component("AppDate", AppDate);
 
 Vue.config.productionTip = false;
